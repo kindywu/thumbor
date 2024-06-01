b52b1356f33607397d1a8cb58145a585acedef6c
kindywu <kindywu@outlook.com>
2024-06-01 15:26:49 +0800

    commit

---
+++ examples/print_test_url.rs
@@ -0,0 +1,17 @@
+use percent_encoding::{percent_encode, NON_ALPHANUMERIC};
+use thumbor::*;
+
+fn main() {
+    print_test_url("https://images.pexels.com/photos/1562477/pexels-photo-1562477.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
+}
+
+fn print_test_url(url: &str) {
+    use std::borrow::Borrow;
+    let spec1 = Spec::new_resize(500, 800, resize::SampleFilter::CatmullRom);
+    let spec2 = Spec::new_watermark(20, 20);
+    let spec3 = Spec::new_filter(filter::Filter::Marine);
+    let image_spec = ImageSpec::new(vec![spec1, spec2, spec3]);
+    let s: String = image_spec.borrow().into();
+    let test_image = percent_encode(url.as_bytes(), NON_ALPHANUMERIC).to_string();
+    println!("test url: http://localhost:3000/image/{}/{}", s, test_image);
+}
